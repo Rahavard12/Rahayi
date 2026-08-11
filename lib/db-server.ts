87d83f302affbdd76a44be5cbaabfad9e69fa832
Rahavard12 <farbod111@outlook.com>
2026-08-11 22:37:52 +0200

    Add server database reader

---
+++ lib/db-server.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+export function serverDb(){
+  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!, {auth:{persistSession:false,autoRefreshToken:false,detectSessionInUrl:false}})
+}
